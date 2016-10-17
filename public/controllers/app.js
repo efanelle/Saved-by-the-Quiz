751a85debfade5c76c286f8672d714ade1dd8d26
Erica Fanelle <efanelle@gmail.com>
2016-10-17 16:50:22 -0400

    creating templates for quiz

---
+++ public/controllers/app.js
@@ -0,0 +1,10 @@
+var app = angular.module('sbtbApp', [])
+
+.factory('quiz', function($http) {
+
+var newUser = function(user) {
+
+};
+
+
+});
--- public/controllers/controller.js
+++
@@ -1,3 +0,0 @@
-function AppCtrl() {
-  console.log('controller is working');
-}
