9b80793da801759461fbf90ac372bea841c7ef84
Erica Fanelle <efanelle@gmail.com>
2016-10-17 15:29:02 -0400

    created array of trivia data

---
+++ triviaData.js
@@ -0,0 +1,43 @@
+var questions = [{
+  question: "What score did Zack get on his SATs?",
+  answers: [900, 1020, 1502, 1600],
+  correct: 2
+}, {
+  question: "What is the name of Screech's robot?",
+  incorrect: ["Mike","Kevin", "Harry", "Slater"],
+  correct: 1
+}, {
+  question: "What was Slater's football jersey number?",
+  incorrect: [6, 9, 12, 11],
+  correct: 0
+}, {
+  question: "Finish this line: \n I'm so excited, I'm so excited, I'm so.....",
+  incorrect: ["drunk", "tired", "excited", "scared"],
+  correct: 3
+}, {
+  question: "Which is NOT a name of an employee at the Max?",
+  incorrect: ['James', 'Jeff', 'Kelly', 'Bruce'],
+  correct: 3
+}, {
+  question: "What was name of the band the group created?",
+  incorrect: ['Zack Attack', 'Friends Forever', 'The Max', 'The Temptations'],
+  correct: 0
+}, {
+  question: "What was Screech's dog's name?",
+  incorrect: ['Max', 'Walter', 'Hounddog', 'Sparky'],
+  correct: 2
+}, {
+  question: "What is Lisa's alias on the Teen Line in 1-900 Crushed?",
+  incorrect: ['Jewel', 'Princess', 'Angel', 'Peaches'],
+  correct: 1
+}, {
+  question: "What is Jessie Spano's middle name?",
+  incorrect: ['Marie', 'Ann', 'Myrtle', 'Christine'],
+  correct: 2
+}, {
+  question: "How much money did Lisa charge on her Dad's credit card?",
+  incorrect: ['$394', '$375', '$342', '$386'],
+  correct: 3
+}];
+
+
