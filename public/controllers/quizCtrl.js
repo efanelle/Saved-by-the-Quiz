63100a905c1cbf1e9b7131ff3c7bac233a27f192
Erica Fanelle <efanelle@gmail.com>
2016-10-17 18:05:31 -0400

    inserted quiz data into factory.

---
+++ public/controllers/quizCtrl.js
@@ -0,0 +1,20 @@
+angular.module('sbtbApp.quiz', [])
+
+.controller('quiz', function(Questions) {
+
+  $scope = {};
+
+var getQuestion = function() {
+
+};
+
+var start= function() {
+  $scope.id = 0;
+  $
+};
+
+var answer = function() {
+
+};
+
+})
--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var express =  require('express');
 var app = express();
 
 
-app.use(express.static(__dirname + '/public'));
-
-
-
+// app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
 
 
 var port = process.env.PORT || 3000;
