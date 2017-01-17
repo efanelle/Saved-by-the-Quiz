c1b3c7630bd3723566300104b431babf62acb9ea
Erica Fanelle <efanelle@gmail.com>
2017-01-16 21:48:51 -0500

    Update homepage styling

--- public/app/resultsView/resultsView.js
+++ public/app/resultsView/resultsView.js
@@ -1,7 +1,6 @@
 angular.module('sbtbApp.results', [])
 
 .controller('ResultsController', function($scope, $location, Questions, users) {
-
   $scope.getUsers = function() {
     users.getScores().then(function(resp) {
       resp.data.forEach(function(user) {
